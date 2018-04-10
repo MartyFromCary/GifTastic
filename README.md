b82c9712a76478fca7377457f382294d57ed0e5a
Elias A Martinez <MartyFromCary@gmail.com>
2018-04-10 12:55:48 -0400

    Clean Up

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # GifTastic
+
+App uses Ajax and API-giphy to retrieve gifs, both still and animated.
+Presented dynamically with jQuery.
+Fun exercise.
--- javascript.js
+++ javascript.js
@@ -1,74 +1,74 @@
 "use strict";
 
 const topics = [
-    'Blade Runner',
-    'Felix the Cat',
-    'Winston Churchill',
-    'Mickey Mouse'
+  "Blade Runner",
+  "Felix the Cat",
+  "Winston Churchill",
+  "Mickey Mouse"
 ];
 
-var divButtons
+var divButtons;
 var addTopic;
 var divImages;
 
 function loadImages(name) {
-    $.ajax({
-            url: "https://api.giphy.com/v1/gifs/search?limit=10&api_key=dc6zaTOxFJmzC&q=" +
-                name.replace(/\s/g, '+'),
-            method: 'GET'
-        })
-        .done(function(response) {
-            divImages.empty();
-            response.data.forEach(function(x) {
-                divImages
-                .append(
-                    $("<div>")
-                    .append(
-                    	$("<h3>")
-                    		.text("Rating: " + x.rating)
-                    		)
-                    .append(
-                    		$("<img>")
-                    			.attr("src", x.images.original_still.url)
-                    			.attr("alt", x.images.original.url)
-                    			)
-                	)
-            });
-            $("img").click(function() {
-                var src = $(this).attr("src");
-                var alt = $(this).attr("alt");
+  $.ajax({
+    url:
+      "https://api.giphy.com/v1/gifs/search?limit=10&api_key=dc6zaTOxFJmzC&q=" +
+      name.replace(/\s/g, "+"),
+    method: "GET"
+  }).done(function(response) {
+    divImages.empty();
+    response.data.forEach(function(x) {
+      divImages.append(
+        $("<div>")
+          .append($("<h3>").text("Rating: " + x.rating))
+          .append(
+            $("<img>")
+              .attr("src", x.images.original_still.url)
+              .attr("alt", x.images.original.url)
+          )
+      );
+    });
+    $("img").click(function() {
+      var src = $(this).attr("src");
+      var alt = $(this).attr("alt");
 
-                $(this).attr("src", alt).attr("alt", src);
-            });
-        });
+      $(this)
+        .attr("src", alt)
+        .attr("alt", src);
+    });
+  });
 }
 
 $(function() {
-    divButtons = $("#divButtons");
-    addTopic = $("#addTopic")
-    divImages = $("#divImages");
+  divButtons = $("#divButtons");
+  addTopic = $("#addTopic");
+  divImages = $("#divImages");
 
-    topics.forEach(function(text) {
-        divButtons.append($("<button>").text(text));
-    });
+  topics.forEach(function(text) {
+    divButtons.append($("<button>").text(text));
+  });
 
-    addTopic.click(function(event) {
-        event.preventDefault();
-        var text = $("#topic-input").val().replace(/^\s+|\s+$/g, '').replace(/\s\s+/g, ' ');
+  addTopic.click(function(event) {
+    event.preventDefault();
+    var text = $("#topic-input")
+      .val()
+      .replace(/^\s+|\s+$/g, "")
+      .replace(/\s\s+/g, " ");
 
-        if (text == "") {
-            return;
-        }
+    if (text == "") {
+      return;
+    }
 
-        divButtons.append($("<button>").text(text));
-
-        $("button").click(function() {
-            loadImages($(this).text());
-        });
-    });
+    divButtons.append($("<button>").text(text));
 
     $("button").click(function() {
-        loadImages($(this).text());
+      loadImages($(this).text());
     });
+  });
 
+  $("button").click(function() {
+    loadImages($(this).text());
+  });
 });
